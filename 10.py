98820f63b072943b3468a50c5996241a525d6209
David Feinblum <dvfeinblum@gmail.com>
2020-12-12 22:30:13 -0500

    day 10 pt 2

--- 10.py
+++ 10.py
@@ -10,19 +10,14 @@ def compute_joltages(adapter_lst):
 
         if idx == 0:
             current_joltage = joltage
-            perm_lst[idx] = 0
-            continue
+            perm_lst[idx] = 1
 
-        choices = 0
         diff = joltage - current_joltage
 
         # look ahead to see if we have other choices
-        if idx < len(adapter_lst) - 1 and adapter_lst[idx + 1] - current_joltage <= 3:
-            choices += 1 
-            perm_lst[idx+1] += 1
-        if idx < len(adapter_lst) - 2 and adapter_lst[idx + 2] - current_joltage <= 3:
-            choices += 1
-            perm_lst[idx+2] += 1
+        for offset in range(1,4):
+            if idx + offset < len(adapter_lst) and adapter_lst[idx + offset] - joltage < 4:
+                perm_lst[idx + offset] += perm_lst[idx]
 
         # calculate diffs
         if diff == 1:
@@ -31,14 +26,13 @@ def compute_joltages(adapter_lst):
             three_jolt_diffs += 1
 
         current_joltage = joltage
-        perm_lst[idx] += choices + perm_lst[idx - 1]
 
     print(f"3 Jolt Diffs: {three_jolt_diffs}\n1 Jolt Diffs: {one_jolt_diffs}\nProduct: {one_jolt_diffs * three_jolt_diffs}")
     print(f"Choices: {perm_lst}")
 
 if __name__ == "__main__":
     #with open("test10_little.txt", "r") as f:
-    with open("test10.txt", "r") as f:
-    #with open("input10.txt", "r") as f:
+    # with open("test10.txt", "r") as f:
+    with open("input10.txt", "r") as f:
         outputs = [int(line) for line in f.readlines()]
     compute_joltages(sorted(outputs))
